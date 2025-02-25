78dbea8753192dc1e5d87f16da7767087582f422
brayan@240211 <laisdelfim@gmail.com>
2025-02-24 21:40:26 -0300

    tarefa ebac

---
+++ test/java/br/com/rpires/Assert.java
@@ -0,0 +1,23 @@
+package br.com.rpires;
+
+import br.com.rpires.domain.Cliente;
+import br.com.rpires.domain.Produto;
+
+public class Assert {
+
+  public static void assertTrue(Boolean retorno1) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'assertTrue'");
+  }
+
+  public static void assertNull(Produto produtoDB) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'assertNull'");
+  }
+
+  public static void assertEquals(String string, String nome) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'assertEquals'");
+  }
+
+}
---
+++ test/java/br/com/rpires/Before.java
@@ -0,0 +1,5 @@
+package br.com.rpires;
+
+public @interface Before {
+
+}
